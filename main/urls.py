2763b3010b9cb619f2c8d2dc18f1cb62ea263035
Nadekoooo <christianyudistira09@gmail.com>
2024-09-18 09:19:04 +0700

    adding new urls and views

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,9 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.show_att, name = "show_att"),
-    path('create-show-form/', views.create_show_form, name='create_show_form')
+    path('create-show-form/', views.create_show_form, name='create_show_form'),
+    path('xml/', views.show_xml, name='show_xml'),
+    path('json/', views.show_json, name='show_json'),
+    path('xml/<str:id>/', views.show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', views.show_json_by_id, name='show_json_by_id')
 ]
--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from main.forms import SuiseiMainForm
 from main.models import ProductEntry
+from django.core import serializers
 
 # Create your views here.
 def show_att(request):
@@ -24,3 +25,19 @@ def create_show_form(request):
     
     context = {'form': form}
     return render(request, "create_show_form.html", context)
+
+def show_xml(request):
+    data = ProductEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = ProductEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
